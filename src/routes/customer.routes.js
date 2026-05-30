6e1722b47c70a9af18388fc72548c5eda707eee9
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2026-05-30 20:59:56 +0600

    feat: implement customer routes with RBAC middleware integration

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -13,10 +13,10 @@ router.get('/top', rbac('customers', 'view'), customerController.getTopCustomers
 router.get('/leaderboard', rbac('customers', 'view'), customerController.getCustomerLeaderboard);
 router.post('/bulk-import', rbac('customers', 'create'), customerController.bulkImport);
 router.get('/phone/:phone', rbac('customers', 'view'), customerController.getCustomerByPhone);
-router.get('/:id', rbac('customers', 'view'), customerController.getCustomer);
-router.put('/:id', rbac('customers', 'update'), customerController.updateCustomer);
-router.delete('/:id', rbac('customers', 'delete'), customerController.deleteCustomer);
-router.post('/:id/collect-due', rbac('customers', 'update'), customerController.collectDue);
-router.get('/:id/history', rbac('customers', 'view'), customerController.getCustomerHistory);
+router.get('/:id([0-9a-fA-F]{24})', rbac('customers', 'view'), customerController.getCustomer);
+router.put('/:id([0-9a-fA-F]{24})', rbac('customers', 'update'), customerController.updateCustomer);
+router.delete('/:id([0-9a-fA-F]{24})', rbac('customers', 'delete'), customerController.deleteCustomer);
+router.post('/:id([0-9a-fA-F]{24})/collect-due', rbac('customers', 'update'), customerController.collectDue);
+router.get('/:id([0-9a-fA-F]{24})/history', rbac('customers', 'view'), customerController.getCustomerHistory);
 
 module.exports = router;
